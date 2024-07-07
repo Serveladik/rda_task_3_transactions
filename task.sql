b2d5cc09c66693bc16c0512b46e8797240891b7a
Serveladus <serveladik@gmail.com>
2024-07-07 13:07:02 +0300

    Solution9

--- task.sql
+++ task.sql
@@ -1,42 +1,4 @@
-DROP DATABASE IF EXISTS ShopDB;
-CREATE DATABASE ShopDB;
 USE ShopDB;
-
-CREATE TABLE Products (
-    ID INT AUTO_INCREMENT,
-    Name VARCHAR(50),
-    Description VARCHAR(100),
-    Price INT,
-    WarehouseAmount INT,
-    PRIMARY KEY (ID)
-);
-
-CREATE TABLE Customers (
-    ID INT AUTO_INCREMENT,
-    FirstName VARCHAR(50),
-    LastName VARCHAR(50),
-    Email VARCHAR(50),
-    Address VARCHAR(50),
-    PRIMARY KEY (ID)
-);
-
-CREATE TABLE Orders (
-    ID INT AUTO_INCREMENT,
-    CustomerID INT, 
-    Date DATE,
-    PRIMARY KEY (ID),
-    FOREIGN KEY (CustomerID) REFERENCES Customers(ID) ON DELETE SET NULL
-);
-
-CREATE TABLE OrderItems (
-    ID INT AUTO_INCREMENT,
-    OrderID INT, 
-    ProductID INT, 
-    Count INT,
-    PRIMARY KEY (ID),
-    FOREIGN KEY (OrderID) REFERENCES Orders(ID) ON DELETE SET NULL,
-    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE SET NULL
-);
 
 INSERT INTO Products (Name, Description, Price, WarehouseAmount)
     VALUES ("AwersomeProduct", "Product Description", 5, 42);
@@ -44,15 +6,14 @@ INSERT INTO Products (Name, Description, Price, WarehouseAmount)
 INSERT INTO Customers (FirstName, LastName, Email, Address)
     VALUES ("John", "Dou", "j@dou.ua", "far, far away");
 
-START TRANSACTION;
-
-SET @Quantity = 1;
-
--- Insert into Orders table inside the transaction
 INSERT INTO Orders (CustomerID, Date) 
 VALUES (1, "2023-01-01");
 
 SET @OrderID = LAST_INSERT_ID();
+
+START TRANSACTION;
+
+SET @Quantity = 1;
 
 INSERT INTO OrderItems (OrderID, ProductID, Count) 
 VALUES (@OrderID, 1, @Quantity);
